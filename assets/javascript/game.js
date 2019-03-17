32e9e18bc98829178130b036bced12eb2e34a21a
Amanda <amandalragone@gmail.com>
2019-03-17 12:52:00 -0500

    Created vars and added links to css and js in HTML file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+var computerChoice = ["the killers", "mumford and sons", "blink 182", "good charlotte", "coldplay", "audioslave", "linkin park", "simple plan", "eminem", "britney spears", "avril lavigne", "nickelback", "evanescence", "new found glory", "yellowcard", "interpol", "weezer", "the all-american rejects", "jonas brothers", "jimmy eat world", "nsync", "backstreet boys", "spice girls"];
+var wins = 0;
+var guessesRemaining = computerChoice.indexOf("")+1
